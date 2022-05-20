ee4b0631be06ca7a747384103be4ef5e18c3a9f3
Jore Dierckx <jore.dierckx@gmail.com>
2022-05-20 21:45:19 +0200

    fixed hover and info

--- components/Info.vue
+++ components/Info.vue
@@ -1,17 +1,20 @@
 <template>
-  <ul id="ref" class="info" :key="info">
-    <li>Produced on 19/05/2022</li>
-    <li>Activated on 23/05/2022 10:30(:30s)pm {{ date }} {{ time }}</li>
-    <li>Time since activation: 12d:23m:23s</li>
-    <li>
-      <br />
-      <i>
-        “As the present makes way for the future, it leaves us with a past.”
-      </i>
-      <br />
-      — Vaast E.M. Colson
-    </li>
-  </ul>
+  <div>
+    <div id="ghost" class="ghost"></div>
+    <ul id="ref" class="info" :key="info">
+      <li>Produced on 19/05/2022</li>
+      <li>Activated on 23/05/2022 10:30(:30s)pm {{ date }} {{ time }}</li>
+      <li>Time since activation: 12d:23m:23s</li>
+      <li>
+        <br />
+        <i>
+          “As the present makes way for the future, it leaves us with a past.”
+        </i>
+        <br />
+        — Vaast E.M. Colson
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -41,17 +44,18 @@ export default {
   },
   mounted() {
     const ref = document.getElementById("ref");
-    window.addEventListener("mousemove", function (e) {
-      let left = e.clientX;
-      let top = e.clientY;
-      ref.style.left = left + "px";
-      ref.style.top = top + "px";
-    });
     const ghost = document.getElementById("ghost");
-    window.addEventListener("mousemove", () => {
+    //window.addEventListener("mousemove", function (e) {
+    //  let left = e.clientX;
+    //  let top = e.clientY;
+    //  ref.style.left = left + "px";
+    //  ref.style.top = top + "px";
+    //});
+    //const ghost = document.getElementById("ghost");
+    ghost.addEventListener("mousemove", () => {
       ref.classList.add("show");
     });
-    window.addEventListener("mouseout", () => {
+    ghost.addEventListener("mouseout", () => {
       ref.classList.remove("show");
     });
   },
